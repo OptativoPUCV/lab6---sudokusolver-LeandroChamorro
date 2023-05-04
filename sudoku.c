89331a7ad5a083748c25c358227e16d79b5f173e
Replit user <>
2023-05-04 21:37:52 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -45,6 +45,28 @@ void print_node(Node* n){
 
 int is_valid(Node* n){
 
+    for(int i = 0; i<9 ; i++)
+    {
+      int marcados[10] = {0};
+      for (int j= 0; j<9 ;j++)
+      {
+        if(n->sudo[i][j] != 0)
+        {
+          if(marcados[n->sudo[i][j]] ==1)
+          {
+            return 0;
+            
+          }else{
+            marcados[n->sudo[i][j]]=1;
+          }
+        }
+      }
+    }
+
+
+
+
+  
     return 1;
 }
 
