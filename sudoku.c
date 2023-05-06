0f8b44a9dc914600e413a5e3ebb886698bdcc1ac
Replit user <>
2023-05-06 22:57:38 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -44,29 +44,39 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
-
     for(int i = 0; i<9 ; i++)
     {
-      int marcados[10] = {0};
+      int num[10] = {0};
       for (int j= 0; j<9 ;j++)
       {
         if(n->sudo[i][j] != 0)
         {
-          if(marcados[n->sudo[i][j]] ==1)
+          if(num[n->sudo[i][j]] ==1)
           {
             return 0;
             
           }else{
-            marcados[n->sudo[i][j]]=1;
+            num[n->sudo[i][j]]=1;
           }
         }
       }
     }
 
+  for (int j = 0; j < 9; j++) {
+        int num[10] = {0};
+        for (int i = 0; i < 9; i++) {
+            if (n->sudo[i][j] != 0) {
+                if (num[n->sudo[i][j]] == 1) {
+                    return 0;
+                } else {
+                    num[n->sudo[i][j]] = 1;
+                }
+            }
+        }
+    }
 
 
-
-  
+    
     return 1;
 }
 
@@ -85,6 +95,7 @@ List* get_adj_nodes(Node* n){
               new_node->sudo[i][j] = k;
               pushBack(list, new_node);
             }
+          retrun list;
         }
       }
     }
