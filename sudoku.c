d5e1e69ccea8c12ac4ae88b41df2a70b1811bd6c
Replit user <>
2023-05-06 23:24:40 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -134,7 +134,33 @@ int is_final(Node* n){
 }
 
 Node* DFS(Node* initial, int* cont){
-  return NULL;
+  Stack* stack = createStack(); 
+    push(stack, initial); 
+
+    while (!is_empty(stack)) {
+        Node* current = top(stack);
+
+        (*cont)++; 
+
+        if (is_final(current)) {
+            return current; 
+        }
+
+        List* adj_nodes = get_adj_nodes(current); 
+
+        Node* adj_node = first(adj_nodes);
+
+        while (adj_node != NULL) {
+            push(stack, adj_node);
+            adj_node = next(adj_nodes); 
+        }
+
+        pop(stack);
+        free(current);
+        clean(adj_nodes);
+    }
+
+    return NULL;
 }
 
 
